918d740734315d5cb5b035493aca2f414a062341
Rambinintsoa Jonathan <webdev.nathan@gmail.com>
2011-07-05 22:27:53 +0300

    Signed-off-by: Rambinintsoa Jonathan <webdev.nathan@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tilpas::Application.config.secret_token = '1d961d980f9d147c69d72ac8b60e7f0cc71aaef528dd1b08fcb8016245728c5dbdd88b49204b227b910a26044cb3f8d65cc6d49983f25e1a1953b58d4379d97d'
